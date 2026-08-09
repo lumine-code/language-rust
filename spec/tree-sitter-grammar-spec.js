2e02c097eec5225a5b15c4c4d12b8bf7f25fdfc4
Sebastian Balcerowiak <asiloisad.93@gmail.com>
2026-08-09 18:54:21 +0200

    Complete the Lumine ecosystem rebrand

--- spec/tree-sitter-grammar-spec.js
+++ spec/tree-sitter-grammar-spec.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 describe("WASM Tree-sitter Rust grammar", () => {
   beforeEach(async () => {
-    await atom.packages.activatePackage("language-rust");
+    await lumine.packages.activatePackage("language-rust");
   });
 
   it("passes grammar tests", async () => {
